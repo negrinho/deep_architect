d0ed2b2c4a5224256361353399bf91a02a75e043
Daniel Ferreira <dcferreira@users.noreply.github.com>
2018-02-12 17:38:39 +0100

    remove accidentally created files

--- tests/random_name/tb_tensorflow.py
+++
@@ -1,5 +0,0 @@
-
-# add some functionality to work with tensorboard.
-# how to give good names to the variables.
-
-# TODO: do a Tensorboard manager.
--- tests/random_name/tests.py
+++
@@ -1,3 +0,0 @@
-
-# this corresponds to writting a lot of tests for the toolbox. make sure 
-# that they are well matched and are easy to write.
