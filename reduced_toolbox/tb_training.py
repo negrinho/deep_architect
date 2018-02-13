6fdf7c1c7b59a894c93f1d79bac1db071749b86c
Renato Negrinho <negrinho@cs.cmu.edu>
2018-02-13 13:10:52 -0500

    Removed examples and tests.

--- reduced_toolbox/tb_training.py
+++
@@ -1,7 +0,0 @@
-def get_eval_fn(start_fn, train_fn, is_over_fn, end_fn):
-    def eval_fn(d):
-        start_fn(d)
-        while not is_over_fn(d):
-            train_fn(d)
-        end_fn(d)
-    return eval_fn
