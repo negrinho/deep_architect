15996fa650f3088cdcb88812f099d9687705d528
Renato Negrinho <negrinho@cs.cmu.edu>
2018-02-14 06:06:41 -0500

    Created a contrib folder. Cleaned up utils.

--- darch/utils.py
+++ darch/utils.py
@@ -1,20 +1,7 @@
-import numpy as np
 import graphviz
 from six import itervalues, iteritems
 import darch.core as co
-from copy import deepcopy
-import pickle
 
-# TODO: may be worth to think about the right level of complexity for
-# representing computation.
-# add the dimensionality to all terminals.
-
-# TODO: perhaps augment with different values.
-
-
-# NOTE: may require to also talk about the other hyperparameters.
-# NOTE: I should be able to write down the dimension in the input or output dim.
-# this would be done upon instantiation; may change in dynamic frameworks.
 def draw_graph(output_or_module_lst, draw_hyperparameters=False, 
         draw_io_labels=False, graph_name='graph', out_folderpath=None, 
         print_to_screen=True):
@@ -65,10 +52,6 @@ def draw_graph(output_or_module_lst, draw_hyperparameters=False,
                     h.get_name(), 
                     x.get_name(), 
                     label=label, fontsize=edge_fs)
-
-            ## TODO: if has to draw hyperparameters; 
-            # do something about the hyperparameters that are not there.
-
         return False
 
     # generate most of the graph.
@@ -91,17 +74,6 @@ def draw_graph(output_or_module_lst, draw_hyperparameters=False,
         g.node(s, fontsize=h_fs)
 
     g.render(graph_name, out_folderpath, view=print_to_screen)  
-
-def copy_graph(output_or_module_lst):
-    return deepcopy(output_or_module_lst)
-
-def save_graph(output_or_module_lst, fpath):
-    with open(fpath, 'wb') as f:
-        pickle.dump(output_or_module_lst, f)    
-
-def load_graph(fpath):
-    with open(fpath, 'rb') as f:
-        return pickle.load(f)
 
 def get_unconnected_inputs(output_or_module_lst):
     module_lst = co.extract_unique_modules(output_or_module_lst)
@@ -129,28 +101,6 @@ def get_unconnected_outputs(input_or_module_lst):
     co.forward_traverse(module_lst, fn)
     return ox_lst
 
-# TODO: needs to be done in all parts of the model. perhaps add more info.
-def propagate_outputs(module):
-    for ox in itervalues(module.outputs):
-        for ix in ox.get_connected_inputs():
-            ix.val = ox.val
-
 def connect_sequentially(module_lst):
     for x, x_next in zip(module_lst[:-1], module_lst[1:]):
         x.outputs['Out'].connect(x_next.inputs['In'])
-
-# TODO: can be extended for the case where there are multiple outputs.
-def module_to_io(m):
-    return (m.inputs, m.outputs) 
-
-def module_lst_to_io(m_lst):
-    return (m_lst[0].inputs, m_lst[-1].outputs)
-
-def module_fn_to_io_fn(fn):
-    return lambda : module_to_io( fn() )
-
-def module_lst_fn_to_io_fn(fn):
-    return lambda : module_lst_to_io( fn() )
-
-def running_max(vs):
-    return np.maximum.accumulate(vs)
