742b403f5bf4477b1b8e773cc2e5044fbb2e289f
Daniel Ferreira <dcferreira@users.noreply.github.com>
2018-03-27 14:31:22 +0200

    added some docs for pytorch helpers

--- darch/helpers/pytorch.py
+++ darch/helpers/pytorch.py
@@ -2,9 +2,22 @@ from six import iteritems
 import darch.core as co
 import torch.nn as nn
 
+
 class PyTModule(co.Module):
+    """
+    Pytorch wrapper for darch module.
+    """
     def __init__(self, name, name_to_hyperp, compile_fn, 
             input_names, output_names, scope=None):
+        """
+        :type name: str
+        :type name_to_hyperp: dict[str,darch.core.Hyperparameter]
+        :type compile_fn: (dict[str,darch.core.Input], dict[str,darch.core.Hyperparameter]) ->
+                          ((dict[str,darch.core.Input]) -> dict[str,darch.core.Output], list[nn.Module])
+        :type input_names: list[str]
+        :type output_names: list[str]
+        :type scope: darch.core.Scope | None
+        """
         co.Module.__init__(self, scope, name)
         self._register(input_names, output_names, name_to_hyperp)
         self._compile_fn = compile_fn
@@ -21,31 +34,46 @@ class PyTModule(co.Module):
         output_name_to_val = self._fn(input_name_to_val)
         self._set_output_values(output_name_to_val)
 
+
 # NOTE: this is done for the case where all the PyTorch modules are created 
 # using the helper here described, i.e., it assumes the existence of pyth_modules.
 def _call_fn_on_pytorch_module(output_lst, fn):
+    """
+    :type output_lst: collections.Iterable[darch.core.Output]
+    :type fn: (darch.core.Module) -> None
+    """
     def fn_iter(mx):
         for pyth_m in mx.pyth_modules:
             fn(pyth_m)
         return False
     co.traverse_backward(output_lst, fn_iter)
 
+
 def get_pytorch_modules(output_lst):
     all_modules = set()
     _call_fn_on_pytorch_module(output_lst, all_modules.add)
     return all_modules
 
+
 def train(output_lst):
+    """Applies :meth:`nn.Module.train` to all modules needed to compute the given outputs."""
     _call_fn_on_pytorch_module(output_lst, lambda pyth_m: pyth_m.train())
 
+
 def eval(output_lst):
+    """Applies :meth:`nn.Module.eval` to all modules needed to compute the given outputs."""
     _call_fn_on_pytorch_module(output_lst, lambda pyth_m: pyth_m.eval())
 
+
 def cuda(output_lst):
+    """Applies :meth:`nn.Module.cuda` to all modules needed to compute the given outputs."""
     _call_fn_on_pytorch_module(output_lst, lambda pyth_m: pyth_m.cuda())
 
+
 def cpu(output_lst):
+    """Applies :meth:`nn.Module.cpu` to all modules needed to compute the given outputs."""
     _call_fn_on_pytorch_module(output_lst, lambda pyth_m: pyth_m.cpu())
+
 
 def parameters(output_lst):
     pyth_modules = get_pytorch_modules(output_lst)
@@ -54,8 +82,16 @@ def parameters(output_lst):
         ps.update(pyth_m.parameters())
     return ps
 
+
 class PyTNetContainer(nn.Module):
+    """
+    Darch wrapper for pytorch modules.
+    """
     def __init__(self, name_to_input, name_to_output):
+        """
+        :type name_to_input: dict[str,darch.core.Input]
+        :type name_to_output: dict[str,darch.core.Output]
+        """
         nn.Module.__init__(self)        
 
         self.name_to_output = name_to_output
@@ -72,10 +108,10 @@ class PyTNetContainer(nn.Module):
         if self._module_seq is None:
             self._module_seq = co.determine_module_eval_seq(self.name_to_input.values())
 
-        input_name_to_val = {ix : name_to_val[name] 
+        input_name_to_val = {ix: name_to_val[name]
             for name, ix in iteritems(self.name_to_input)}
         co.forward(input_name_to_val, self._module_seq)
-        output_name_to_val = {name : ox.val 
+        output_name_to_val = {name: ox.val
             for name, ox in iteritems(self.name_to_output)}
 
         if not self._is_compiled:
