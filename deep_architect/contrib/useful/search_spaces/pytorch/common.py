8a1e65453d0adf025d87af4ebb1292fe16495b9d
Renato Negrinho <negrinho@cs.cmu.edu>
2018-09-01 19:21:30 -0400

    Removed the placeholders for PyTorch search spaces.

--- deep_architect/contrib/useful/search_spaces/pytorch/common.py
+++
@@ -1,8 +0,0 @@
-import deep_architect.helpers.pytorch as hpt
-import deep_architect.hyperparameters as hp
-
-# Alias discrete hyperparameters for brevity. s
-D = hp.Discrete
-
-def siso_pytorch_module(name, compile_fn, name_to_hyperp, scope=None):
-    return hpt.PyTorchModule(name, name_to_hyperp, compile_fn, ['In'], ['Out'], scope).get_io()
