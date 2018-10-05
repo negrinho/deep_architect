9e04e9c64cb581984e166aea65dedf7aa98a5971
Renato Negrinho <negrinho@cs.cmu.edu>
2018-10-05 14:04:29 -0400

    Minor update to simplest multiworker.

--- examples/simplest_multiworker/run.sh
+++ examples/simplest_multiworker/run.sh
@@ -4,19 +4,16 @@ if [ $# -ne 2 ]; then
 fi
 cfg_name="$1"
 num_workers=$2
-folderpath=dev/negrinho/multiworker/simplest
-cfg_filepath="$folderpath/configs/$cfg_name.json"
 # generate architectures
 ipython $folderpath/master.py -- \
-    --config_filepath "$cfg_filepath"
-
+    --config_filepath "examples/simplest_multiworker/configs/$cfg_name.json"
+# run the workers
 u=$(($num_workers - 1))
 for worker_id in $(seq 0 $u); do
-    ipython $folderpath/worker.py -- \
-        --config_filepath "$cfg_filepath" \
+    ipython examples/simplest_multiworker/worker.py -- \
+        --config_filepath "examples/simplest_multiworker/configs/$cfg_name.json" \
         --worker_id $worker_id \
         --num_workers $num_workers &
 done
 
 # To run with GPU support, use export CUDA_VISIBLE_DEVICES=$worker_id
-# TODO: it is not clear that it is using the GPU.
